3b092ce2f70aa25e79a7444aae09928078a570b0
shally3009 <mittalshally30@gmail.com>
2025-04-23 14:51:05 +0530

    Added post api

--- backend/src/Routes/userRoutes.js
+++ backend/src/Routes/userRoutes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const User = require('../Model/User');
 
 // Register User
-// router.post('/register', async (req, res) => {
-//   try {
-//     const user = await User.create(req.body);
-//     res.status(201).json(user);
-//   } catch (err) {
-//     res.status(400).json({ error: err.message });
-//   }
-// });
+router.post('/register', async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
 
 // Get User by ID
 router.get('/:id', async (req, res) => {
