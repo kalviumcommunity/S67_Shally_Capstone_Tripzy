945a1e0030c8704d48f56c339848101bbee58d54
shally3009 <mittalshally30@gmail.com>
2025-04-24 04:05:56 +0530

    implemented-relationship-between-entities-in-database

--- backend/src/Model/User.js
+++ backend/src/Model/User.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+// models/User.js
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: {type: String},
-  email: { type: String, unique: true },
-  password: {type: String},
-  profilePic: String,
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profileImage: { type: String },
 }, { timestamps: true });
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
