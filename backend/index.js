0e6fd3cf43d0a3144b83d05cdf20e633679d2604
shally3009 <mittalshally30@gmail.com>
2025-04-24 17:30:03 +0530

    fileupload

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./src/Database/db');
+const uploadRoutes = require('./src/Routes/uploadRoutes');
 
 dotenv.config({
     path: './src/config/.env'
