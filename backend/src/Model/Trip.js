bb479e853795b185b79660a758adaed309083b52
shally3009 <mittalshally30@gmail.com>
2025-04-25 13:01:31 +0530

    implemented authentication

--- backend/src/Model/Trip.js
+++ backend/src/Model/Trip.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 
 const tripSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  title: { type: String, required: true },
   destination: { type: String, required: true },
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: true },
